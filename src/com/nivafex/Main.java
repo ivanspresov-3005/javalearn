b963e972e9b222878b124aa091922bc067854b61
Ivan Spresov <Ivan_Spresov@epam.com>
2020-05-12 21:23:51 +0300

    firsttask

---
+++ src/com/nivafex/Main.java
@@ -0,0 +1,10 @@
+package com.nivafex;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Человек и робот друзья навек");
+        System.out.println("Человек и робот друзья навек");
+        System.out.println("Человек и робот друзья навек");
+    }
+}
