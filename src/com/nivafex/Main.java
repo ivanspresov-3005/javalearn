35a470eac7e393e0cddc1e05c1ccdfe5d194e2ce
Goncharik Andrey <goncharikandrey@gmail.com>
2020-05-14 01:28:58 +0300

    hello

--- src/com/nivafex/Main.java
+++ src/com/nivafex/Main.java
@@ -3,8 +3,6 @@ package com.nivafex;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Человек и робот друзья навек");
-        System.out.println("Человек и робот друзья навек");
-        System.out.println("Человек и робот друзья навек");
+        System.out.println("Hello, world");
     }
 }
